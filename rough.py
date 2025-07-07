aa1c7032435f949354d2d37f3a172ec4925bed2a
harshteckchandani1 <lt.aids1@gsv.ac.in>
2025-07-07 12:57:56 +0530

    Enclapsulation, getter setter and static method covered

--- rough.py
+++ rough.py
@@ -15,11 +15,28 @@ from oops_py_project import chatbook
 
 # obj = chatbook()
 
-lst = [1,2,3]
+# lst = [1,2,3]
 
 # function
-a1= len(lst)
-print(a1)
+# a1= len(lst)
+# print(a1)
 
 # calling method
+# user1 = chatbook()
+# user1.msg_friend()
+
+# getter setter
+# print(user1.get_name())
+# user1.set_name("Agent X")
+# print(user1.get_name())
 user1 = chatbook()
+print(user1.id)
+
+# using static method directly from class rather than obj
+
+chatbook.set_id(10)
+user2 = chatbook()
+print(user2.id)
+
+user3 = chatbook()
+print(user3.id)
