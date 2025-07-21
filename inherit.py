aa763d62c7c6faa5b11f9fe835ed61a8c8c9215f
HarshTeckchandani31 <harsh31121995@gmail.com>
2025-07-22 00:13:30 +0530

    21/7/2025

--- inherit.py
+++ inherit.py
@@ -1,28 +1,55 @@
-# simple inheritance
+# # simple inheritance
 
-# Base class/ parent class
+# # Base class/ parent class
 
+# class Animal:
+#     def __init__(self, name):
+#         self.__name = name
+    
+#     def speak(self):
+#         print(f"{self.__name} makes a sound.")
+
+# # derived class
+
+# class Dog:
+#     def __init__(self):
+#         self.behaviour ="friendly"
+
+#     def speak(self):
+#         print(f"{self.name} barks. he is very {self.behaviour} ")
+
+# # Create an instance of class Animal
+# animal = Animal("Generic Animal")
+# animal.speak() # Output: Generic Animal makes a sound.
+
+# # create an instance of class dog
+
+# dog = Dog("Buddy")
+# dog.speak() # Output: Buddy barks
+
+# super keyword
+# Base class
 class Animal:
-    def __init__(self, name):
-        self.__name = name
+    def __init__(self):
+        self.name ="Buddy"
     
     def speak(self):
-        print(f"{self.__name} makes a sound.")
+        print (f"{self.name} makes a sound.")
 
-# derived class
-
-class Dog:
-    def __init__(self):
-        self.behaviour ="friendly"
+# Derived class
+class Dog(Animal):
+    def __init__(self, breed):
+        super().__init__()
+        self.breed = breed
 
     def speak(self):
-        print(f"{self.name} barks. he is very {self.behaviour} ")
+        super().speak() # Call the base class method
+        print(f"{self.name} barks. It is a {self.breed}.")
 
-# Create an instance of class Animal
-animal = Animal("Generic Animal")
-animal.speak() # Output: Generic Animal makes a sound.
+# create an instance of Dog 
 
-# create an instance of class dog
-
-dog = Dog("Buddy")
-dog.speak() # Output: Buddy barks
+dog = Dog("Golden Retriver")
+dog.speak()
+# Output
+# Buddy makes a sound
+# Buddy barks. It is a Golden Retriver
